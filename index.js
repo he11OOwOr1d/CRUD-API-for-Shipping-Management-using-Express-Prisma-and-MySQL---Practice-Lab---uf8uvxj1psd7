b848907a423451e482a5d4be5fbb6bd71bc3e323
Nikhil Raj <nikhilraj@Nikhils-MacBook-Pro.local>
2024-12-15 17:31:32 +0530

    all api

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 3001;
 
 app.post('/api/shipping/create', async(req, res) => {
   const {userId, productId, count} = req.body;
+  if(!userId || !productId || !count){
+    return res.status(404).json({
+      error: "All fields required"
+    })
+  }
   const create = await prisma.shipping.create({
     data: {
       userId,
@@ -21,8 +26,13 @@ app.post('/api/shipping/create', async(req, res) => {
   return res.status(201).json(create);
 });
 
-app.put("/api/shipping/cancel", async(req,res)=>{
+app.put('/api/shipping/cancel', async(req,res)=>{
   const {shippingId} = req.body
+  if (!shippingId){
+    return res.status(404).json({
+      error: "Missing shippingId"
+    })
+  }
   const cancel = await prisma.shipping.update({
     where: {id: shippingId},
     data:{
@@ -30,6 +40,18 @@ app.put("/api/shipping/cancel", async(req,res)=>{
     }
   })
   return res.status(200).json(cancel)
+})
+
+app.get('/api/shipping/get', async(req,res)=>{
+  const {userId} = req.query;
+  if(!userId){
+  const get = await prisma.shipping.findMany()
+  return res.status(200).json(get)
+  }
+  const hi = await prisma.shipping.findMany({
+    where: {userId: Number(userId)}
+  })
+  return res.status(200).json(hi)
 })
 
 app.listen(PORT, () => {
