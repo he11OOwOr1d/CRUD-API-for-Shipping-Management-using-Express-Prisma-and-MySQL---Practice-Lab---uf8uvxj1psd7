6bcec32a009cc22afa3cb715c5ca9f005304668d
Nikhil Raj <nikhilraj@Nikhils-MacBook-Pro.local>
2024-12-15 17:35:55 +0530

    tried once  agian

--- migrations/20241215120512_shipho/migration.sql
+++ migrations/20241215120512_shipho/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Shipping` (
     `userId` INTEGER NOT NULL,
     `productId` INTEGER NOT NULL,
     `count` INTEGER NOT NULL,
-    `status` VARCHAR(191) NOT NULL DEFAULT 'Pending',
+    `status` VARCHAR(191) NOT NULL DEFAULT 'pending',
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
