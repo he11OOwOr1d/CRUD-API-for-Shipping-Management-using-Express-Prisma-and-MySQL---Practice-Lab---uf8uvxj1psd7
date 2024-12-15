a008fb51ffef987bc20acea621f9f8352cf4b934
Nikhil Raj <nikhilraj@Nikhils-MacBook-Pro.local>
2024-12-15 17:56:31 +0530

    tried once  agian

--- verifySecret.js
+++ verifySecret.js
@@ -3,14 +3,14 @@ const SHIPPING_SECRET_KEY = process.env.SHIPPING_SECRET_KEY;
 const verifySecret = (req, res, next) => {
   const headerSecret = req.headers['shipping-secret-key'];
   
-  
+  console.log(!headerSecret)
   if (!headerSecret) {
+    console.log("headerSecret is missing")
     return res.status(403).json({ 
       "error": "SHIPPING_SECRET_KEY is missing or invalid"
     });
   }
-  
-  if (SHIPPING_SECRET_KEY !== headerSecret) {
+  else if (SHIPPING_SECRET_KEY !== headerSecret) {
     return res.status(403).json({ 
       "error": "Failed to authenticate SHIPPING_SECRET_KEY"
     });
